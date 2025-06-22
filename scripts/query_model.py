e7a13cec71dd4c96dbf8a84f121dedae4a0d561c
Juan Cardenas <cardenas.juan93@yahoo.com>
2025-06-22 00:54:33 -0700

    Adding capability to querey GhatGPT4.1 on a countrry by country basis given one has an OpenAI API key and credits., Saved the output of the response to searching for relevant links for Mexico

--- scripts/query_model.py
+++ scripts/query_model.py
@@ -1,6 +1,27 @@
 import argparse
+from openai import OpenAI
 from propeterra_internship_2025.data.all_countries import regions
 from propeterra_internship_2025.utils.prompt_templates import Prompt
+
+
+gpt_model = ["gpt-4.1","o4-mini"]
+
+
+def call_model(model, country, prompt):
+
+    with open(f"model_output/{model}_{country}.txt", "a", encoding="utf8") as file:
+
+        if model in gpt_model and prompt != "":
+            client = OpenAI() # API key is obtained automatically by setting your OPENAI_API_KEY environment variable to your API key
+            response = client.responses.create(
+                model=model,
+                tools=[{"type": "web_search_preview"}],
+                input=prompt
+            )
+            file.write(response.output_text)
+            print(response.output_text)
+        else:
+            print("something went wrong")
 
 def main(args):
     '''
@@ -10,13 +31,26 @@ def main(args):
     # Pre-defined Region of interest, can choose
     # region_of_interest = "Latin America"
     region_of_interest = args.region
-    
-    for country in regions.get(region_of_interest, "North America")[:2]:
-        prompt = Prompt(num_sources=args.n_sources, country_of_interest=country, prompt_num=2).construct_prompt()
+
+
+    # Initialize prompt
+    prompt = ""
+
+    # Selecting just 1 country
+    if args.country != "":
+        prompt = Prompt(num_sources=args.n_sources, country_of_interest=args.country, prompt_num=args.prompt_num).construct_prompt()
         print(prompt, "\n\n")
+        call_model(args.model, args.country, prompt)
+    else:
+        # Loop over all countries in a region
+        for country in regions.get(region_of_interest, "North America"):
+            prompt = Prompt(num_sources=args.n_sources, country_of_interest=country, prompt_num=args.prompt_num).construct_prompt()
+            print(prompt, "\n\n")
+            # Not calling model yet, stull testing
 
 
-    # Logic to prompt model
+
+
 
 
 if __name__ == "__main__":
@@ -25,7 +59,8 @@ if __name__ == "__main__":
     parser.add_argument("-r","--region", type=str, default="Latin America", choices=regions.keys(), help="The regions that you want to probe, will loop over all countries in region")
     parser.add_argument("-ns","--n_sources", type=int, default=10, help="The number of links you want to get back from the model")
     parser.add_argument("-pn", "--prompt_num", type=int, default=1, help="The prompt template that you want to submit to the model. List of all prompts is located in 'prompt templates'")
-    parser.add_argument("-m", "--model", type=str, default="perplexity", help="The model that you want to query")
+    parser.add_argument("-c", '--country', type=str, default="Mexico", help="Specify a single country you would like to generate a prompt for")
+    parser.add_argument("-m", "--model", type=str, default="gpt-4.1", choices=["gpt-4.1","perplexity"], help="The model that you want to query")
 
 
     args = parser.parse_args()
--- src/propeterra_internship_2025/utils/prompt_templates.py
+++ src/propeterra_internship_2025/utils/prompt_templates.py
@@ -18,23 +18,42 @@ class Prompt:
         self.template_prototype_1 = "Using the links provided below, can you provide __num_sources__ links for webpages with similar content for the country of __country_of_interest__?\n\n" \
         "__links__\n\nI care about gathering relevant data for the real estate market that contain economic metrics that could affect real estate, how climate could affect the real estate market \n" \
         "governemental factors such as, policy and laws and regulation that could impact real estate market, as well as real estate professionals, and real estate tools that are used in the country \n" \
+        "and or region. The links can have information that is in the county's native language, as well as in English. The links can contain information or have been written between \n" \
+        "the years 2016 and 2025. Please verify that the links you return are accessible. Return your response in a JSON format where the links are categorized in according to data type and link, primarily, \n" \
+        "then use your judgement to create other json keys to categorize the data into."
+
+        # removes the ability to return results in native language
+        self.template_prototype_2 = "Using the links provided below, can you provide __num_sources__ links for webpages with similar content for the country of __country_of_interest__?\n\n" \
+        "__links__\n\nI care about gathering relevant data for the real estate market that contain economic metrics that could affect real estate, how climate could affect the real estate market \n" \
+        "governemental factors such as, policy and laws and regulation that could impact real estate market, as well as real estate professionals, and real estate tools that are used in the country \n" \
         "and or region. The links can have information that is in the county's native language, __native_language__ as well as in English. The links can contain information or have been written between \n" \
         "the years 2016 and 2025. Please verify that the links you return are accessible. Return your response in a JSON format where the links are categorized in according to data type and link, primarily, \n" \
         "then use your judgement to create other json keys to categorize the data into."
 
         # Prompt has a more demanding tone
-        self.template_prototype_2 = "Provide __num_sources__ links for webpages that contain information about the real estate market for the country of __country_of_interest__\n" \
+        self.template_prototype_3 = "Provide __num_sources__ links for webpages that contain information about the real estate market for the country of __country_of_interest__\n" \
         "The links should contain information on real estate trends, economic metrics that could affect real estate, how climate could affect the real estate market \n" \
         "how governemental factors could affect real estate such as, policy and laws and regulation that could impact real estate market, as well as real estate professionals, and real estate tools that are used in the country \n" \
         "and or region. The links can have information that is in the county's native language, __native_language__ as well as in English. The links can contain information or have been written between \n" \
         "the years 2016 and 2025. Please verify that the links you return are accessible. Return your response in a JSON format where the links are categorized in according to data type and link, primarily, \n" \
-        "then use your judgement to create other json keys to categorize the data into."
+        "then use your judgement to create other json keys to categorize the data into. This is prompt number: __prompt_num__"
 
+       # Now includes prompt number
+        self.template_prototype_4 = "Using the links provided below, can you provide __num_sources__ links for webpages with similar content for the country of __country_of_interest__?\n\n" \
+        "__links__\n\nI care about gathering relevant data for the real estate market that contain economic metrics that could affect real estate, how climate could affect the real estate market \n" \
+        "governemental factors such as, policy and laws and regulation that could impact real estate market, as well as real estate professionals, and real estate tools that are used in the country \n" \
+        "and or region. The links can have information that is in the county's native language, as well as in English, and should not be shallow, explore the webpage for paths to relevant data and descriptions.\n" \
+        "The links can consist of but are not limited to articles, downloadable pdfs, pages containing downloadable csv files, pages with graphs or charts representing plotted data, and reports from official agencies.\n" \
+        "The information or have been written between the years 2016 and 2025. Please verify that the links you return are accessible, and do not return a 404 error or otherwise do not return any meaningful content. \n" \
+        "Return your response in a JSON format where the links are categorized in according to data type and link, primarily, \n" \
+        "then use your judgement to create other json keys to categorize the data into. This is prompt number: __prompt_num__"
 
 
         self.prompt_dictionary = {
                                     1:self.template_prototype_1,
                                     2:self.template_prototype_2,
+                                    3:self.template_prototype_3,
+                                    4:self.template_prototype_4,
         }
 
     def construct_prompt(self) -> str:
@@ -57,7 +76,8 @@ class Prompt:
         selected_prompt = selected_prompt.replace('__num_sources__', self.num_sources)
         selected_prompt = selected_prompt.replace('__country_of_interest__', self.country_of_interest)
         selected_prompt = selected_prompt.replace('__native_language__', self.native_language.get(self.country_of_interest, ""))
-        selected_prompt = selected_prompt.replace('__links__', " \n".join(unclean_reference_material[:10]))
+        selected_prompt = selected_prompt.replace('__links__', " \n".join(unclean_reference_material[:24]))
+        selected_prompt = selected_prompt.replace('__prompt_num__', str(self.prompt_num))
 
 
         return selected_prompt
